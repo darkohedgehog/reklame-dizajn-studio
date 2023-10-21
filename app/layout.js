cfbde98e575189dad9e4056e73a662129b2609f8
darkohedgehog <zivic.darko79@gmail.com>
2023-10-21 23:56:19 +0200

    Modified file

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Sora } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const sora = Sora ({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sora.className}>{children}</body>
     </html>
   )
 }
